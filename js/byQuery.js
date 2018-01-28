48c4b7f9b9f2078cf0dfc2d5ef89ec32f7bdcf14
ByronGBP <byrongbp@gmail.com>
2018-01-28 16:47:17 +0100

    added methods for create elements with class or id

--- js/byQuery.js
+++ js/byQuery.js
@@ -3,8 +3,27 @@ function byQuery () {
 
 }
 
+byQuery.prototype.createButtonWithIdAndText = function (id, text) {
+    var button = this.createButton();
+    this.setIdTo(button, id);
+    this.setTextTo(button, text);
+    return button;
+}
+
 byQuery.prototype.createButton = function () {
     return this.createNew('button');
+}
+
+byQuery.prototype.createDivWithClass = function (className) {
+    var div = this.createDiv();
+    this.addClassTo(div, className);
+    return div;
+}
+
+byQuery.prototype.createDivWithId = function (id) {
+    var div = this.createDiv();
+    this.setIdTo(div, id);
+    return div;
 }
 
 byQuery.prototype.createDiv = function () {
@@ -13,6 +32,10 @@ byQuery.prototype.createDiv = function () {
 
 byQuery.prototype.setIdTo = function (elem, id) {
     this.addAttribute(elem, 'id', id);
+}
+
+byQuery.prototype.addClassTo = function (elem, className) {
+    elem.classList.add(className);
 }
 
 byQuery.prototype.createNew = function (elem) {
@@ -37,4 +60,12 @@ byQuery.prototype.addEventClickTo = function (elem, callback){
 
 byQuery.prototype.addEventTo =  function (elem, event, callback) {
     elem.addEventListener(event, callback);
+}
+
+byQuery.prototype.removeEventClickTo = function (elem, callback){
+    this.removeEventTo(elem, 'click', callback);
+}
+
+byQuery.prototype.removeEventTo =  function (elem, event, callback) {
+    elem.removeEventListener(event, callback);
 }
