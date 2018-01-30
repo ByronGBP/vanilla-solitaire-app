1275a0f69dd92b488a636cf4d89e878baff66ccf
ByronGBP <byrongbp@gmail.com>
2018-01-30 22:17:38 +0100

    added var stack, flipped, aceSpace, pileSpace elements

--- js/solitaire/layout.js
+++ js/solitaire/layout.js
@@ -1,6 +1,10 @@
 function Layout () {
   this.containerElement = null;
   this.template = null;
+  this.aceSpaceElement = null;
+  this.pileSpaceElement = null;
+  this.stackCardElement = null;
+  this.flippedCardElement = null;
 
   this._build();
 }
@@ -8,6 +12,15 @@ function Layout () {
 Layout.prototype._build = function () {
   this.containerElement = byQuery.generateDiv('game-element');
   this.template = new SolitaireTemplate(this.containerElement);
+  this._declareElements();
+};
+
+Layout.prototype._declareElements = function () {
+  var leftContainer = byQuery.getChildrenFrom(this.template.topElement, 0);
+  this.stackCardElement = byQuery.getChildrenFrom(leftContainer, 0);
+  this.flippedCardElement = byQuery.getChildrenFrom(leftContainer, 1);
+  this.aceSpaceElement = byQuery.getChildrenFrom(this.template.topElement, 1);
+  this.pileSpaceElement = byQuery.getChildrenFrom(this.template.bottomElement, 0);
 };
 
 Layout.prototype.getButton = function () {
