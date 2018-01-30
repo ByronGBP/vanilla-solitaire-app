a1ea59af3097532604ee065da62a80c745f0d296
ByronGBP <byrongbp@gmail.com>
2018-01-30 21:15:04 +0100

    specify private methods, implemented generateDiv generateButton

--- js/byQuery.js
+++ js/byQuery.js
@@ -3,69 +3,97 @@ function byQuery () {
 
 }
 
-byQuery.prototype.createButtonWithIdAndText = function (id, text) {
-  var button = this.createButton();
-  this.setIdTo(button, id);
-  this.setTextTo(button, text);
+byQuery.prototype.generateButton = function (text, id, className) {
+  var button = this._createButton();
+  this._setTextTo(button, text);
+  this._setIdTo(button, id);
+  this._addClassTo(button, className);
   return button;
 };
 
-byQuery.prototype.createButton = function () {
-  return this.createNew('button');
-};
-
-byQuery.prototype.createDivWithClass = function (className) {
-  var div = this.createDiv();
-  this.addClassTo(div, className);
+byQuery.prototype.generateDiv = function (id, className) {
+  var div = this._createDiv();
+  this._setIdTo(div, id);
+  this._addClassTo(div, className);
   return div;
 };
 
-byQuery.prototype.createDivWithId = function (id) {
-  var div = this.createDiv();
-  this.setIdTo(div, id);
-  return div;
+byQuery.prototype.createDivsWithProgressiveId = function (numberDivs, baseId) {
+  var arrayDivs = [];
+  for (var i = 0; i < numberDivs; i++) {
+    var newDiv = this.generateDiv(baseId + i);
+    arrayDivs.push(newDiv);
+  }
+
+  return arrayDivs;
 };
 
-byQuery.prototype.createDiv = function () {
-  return this.createNew('div');
+byQuery.prototype.appendTo = function (elem, childs) {
+  childs = this._parseToArray(childs);
+
+  for (var i = 0; i < childs.length; i++) {
+    elem.appendChild(childs[i]);
+  }
 };
 
-byQuery.prototype.setIdTo = function (elem, id) {
-  this.addAttribute(elem, 'id', id);
+byQuery.prototype.addEventClickTo = function (elem, callback) {
+  this._addEventTo(elem, 'click', callback);
 };
 
-byQuery.prototype.addClassTo = function (elem, className) {
-  elem.classList.add(className);
-};
-
-byQuery.prototype.createNew = function (elem) {
-  return document.createElement(elem);
-};
-
-byQuery.prototype.setTextTo = function (elem, text) {
-  elem.innerText = text;
-};
-
-byQuery.prototype.addAttribute = function (elem, att, value) {
-  elem.setAttribute(att, value);
+byQuery.prototype.removeEventClickTo = function (elem, callback) {
+  this._removeEventTo(elem, 'click', callback);
 };
 
 byQuery.prototype.getById = function (id) {
   return document.getElementById(id);
 };
 
-byQuery.prototype.addEventClickTo = function (elem, callback) {
-  this.addEventTo(elem, 'click', callback);
+byQuery.prototype._createNew = function (elem) {
+  return document.createElement(elem);
 };
 
-byQuery.prototype.addEventTo = function (elem, event, callback) {
+byQuery.prototype._createButton = function () {
+  return this._createNew('button');
+};
+
+byQuery.prototype._createDiv = function () {
+  return this._createNew('div');
+};
+
+byQuery.prototype._setTextTo = function (elem, text) {
+  if (!text) {
+    text = '';
+  }
+  elem.innerText = text;
+};
+
+byQuery.prototype._setIdTo = function (elem, id) {
+  if (id) {
+    this._setAttribute(elem, 'id', id);
+  }
+};
+
+byQuery.prototype._addClassTo = function (elem, classNames) {
+  if (classNames) {
+    classNames = this._parseToArray(classNames);
+    for (var i = 0; i < classNames.length; i++) {
+      elem.classList.add(classNames[i]);
+    }
+  }
+};
+
+byQuery.prototype._setAttribute = function (elem, att, value) {
+  elem.setAttribute(att, value);
+};
+
+byQuery.prototype._addEventTo = function (elem, event, callback) {
   elem.addEventListener(event, callback);
 };
 
-byQuery.prototype.removeEventClickTo = function (elem, callback) {
-  this.removeEventTo(elem, 'click', callback);
+byQuery.prototype._removeEventTo = function (elem, event, callback) {
+  elem.removeEventListener(event, callback);
 };
 
-byQuery.prototype.removeEventTo = function (elem, event, callback) {
-  elem.removeEventListener(event, callback);
+byQuery.prototype._parseToArray = function (item) {
+  return item.length ? item : [item];
 };
