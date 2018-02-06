f34a8289fe8ee14c583f3789c7ac4c1429de59b0
ByronGBP <byrongbp@gmail.com>
2018-02-06 22:10:20 +0100

    change position

--- js/constants.js
+++ js/constants.js
@@ -2,6 +2,7 @@
 var byQuery = new byQuery();
 
 var CARDS = [
+  { suit: 'spades', value: 'A', color: 'black', point: 1 },
   {suit: 'spades', value: '2', color: 'black', point: 2},
   {suit: 'spades', value: '3', color: 'black', point: 3},
   {suit: 'spades', value: '4', color: 'black', point: 4},
@@ -14,8 +15,8 @@ var CARDS = [
   {suit: 'spades', value: 'J', color: 'black', point: 11},
   {suit: 'spades', value: 'Q', color: 'black', point: 12},
   {suit: 'spades', value: 'K', color: 'black', point: 13},
-  {suit: 'spades', value: 'A', color: 'black', point: 1},
 
+  { suit: 'clubs', value: 'A', color: 'black', point: 1 },
   {suit: 'clubs', value: '2', color: 'black', point: 2},
   {suit: 'clubs', value: '3', color: 'black', point: 3},
   {suit: 'clubs', value: '4', color: 'black', point: 4},
@@ -28,8 +29,8 @@ var CARDS = [
   {suit: 'clubs', value: 'J', color: 'black', point: 11},
   {suit: 'clubs', value: 'Q', color: 'black', point: 12},
   {suit: 'clubs', value: 'K', color: 'black', point: 13},
-  {suit: 'clubs', value: 'A', color: 'black', point: 1},
 
+  { suit: 'diamonds', value: 'A', color: 'red', point: 1 },
   {suit: 'diamonds', value: '2', color: 'red', point: 2},
   {suit: 'diamonds', value: '3', color: 'red', point: 3},
   {suit: 'diamonds', value: '4', color: 'red', point: 4},
@@ -42,8 +43,8 @@ var CARDS = [
   {suit: 'diamonds', value: 'J', color: 'red', point: 11},
   {suit: 'diamonds', value: 'Q', color: 'red', point: 12},
   {suit: 'diamonds', value: 'K', color: 'red', point: 13},
-  {suit: 'diamonds', value: 'A', color: 'red', point: 1},
 
+  { suit: 'hearts', value: 'A', color: 'red', point: 1 },
   {suit: 'hearts', value: '2', color: 'red', point: 2},
   {suit: 'hearts', value: '3', color: 'red', point: 3},
   {suit: 'hearts', value: '4', color: 'red', point: 4},
@@ -55,9 +56,7 @@ var CARDS = [
   {suit: 'hearts', value: '10', color: 'red', point: 10},
   {suit: 'hearts', value: 'J', color: 'red', point: 11},
   {suit: 'hearts', value: 'Q', color: 'red', point: 12},
-  {suit: 'hearts', value: 'K', color: 'red', point: 13},
-  {suit: 'hearts', value: 'A', color: 'red', point: 1}
-
+  {suit: 'hearts', value: 'K', color: 'red', point: 13}
 ];
 
 var TYPE = {
